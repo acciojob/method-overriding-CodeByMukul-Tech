da23831c1cd5a82fd4490cd6b8c3deb6e488025c
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-11-11 23:19:28 +0530

    Complete question

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B b = new B();
+        A aref = new A();
+        System.out.println(aref.meth());
+        System.out.println(b.meth());
+    }
 }
